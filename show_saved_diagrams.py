b42d1f385da98b8d8e05847b08bad4a18313f6c2
Ángel Poc <muxitox>
2024-02-06 10:34:27 +0100

    Add new argument to plotter function

--- show_saved_diagrams.py
+++ show_saved_diagrams.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     positional_embedding_size = 10
 
     # Create variables for the Hopfield Transformer (HT)
-    seed_list = [8]
+    seed_list = [1]
     beta_list = np.linspace(0, 3, 1000)
-    num_feat_patterns_list = [10]
-    num_transient_steps = 256
+    num_feat_patterns_list = [16]
+    num_transient_steps = 800
     max_sim_steps = 1024
     save_not_plot = False
+    reorder_weights = False
     ini_tokens_list = [0]
 
     plotter(num_feat_patterns_list, semantic_embedding_size, positional_embedding_size, beta_list, num_transient_steps,
-            max_sim_steps, ini_tokens_list, seed_list, save_not_plot)
+           max_sim_steps, ini_tokens_list, seed_list, reorder_weights, save_not_plot)
