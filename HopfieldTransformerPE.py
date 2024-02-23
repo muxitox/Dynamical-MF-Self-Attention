fa3f95c8d6badd6325b4ab2561cb5bd2356fbab5
muxitox <angel.poc.lopez@gmail.com>
2024-02-23 15:35:35 +0100

    Start implementing MF in inf. Move utils to utils.py

--- HopfieldTransformerPE.py
+++ HopfieldTransformerPE.py
@@ -1,24 +1,6 @@
 import numpy as np
 from scipy.special import softmax
-
-
-def bool2int(x):  # Transform bool array into positive integer
-    """
-    Transform bool array into positive integer. Code from
-    https://github.com/MiguelAguilera/Adaptation-to-criticality-through-organizational-invariance/blob
-    /dd46c3d272f05becaaf68bef92e724e5c3560150/Network/ising.py#L185
-    :param x: :return:
-    """
-    y = 0
-    for i, j in enumerate(np.array(x)[::-1]):
-        y += j * 2 ** i
-    return int(y)
-
-
-def bitfield(n, size):  # Transform positive integer into bit array
-    x = [int(x) for x in bin(int(n))[2:]]
-    x = [0] * (size - len(x)) + x
-    return np.array(x)
+from utils import bitfield, bool2int
 
 
 class Embedding:
