Fix error in scaling operation performed twice against never

compare_mf_finite_inf.py
@@ -28,13 +28,13 @@ if __name__ == "__main__":
     # vocab.initialize()
 
     # Create variables for the Hopfield Transformer (HT)
-    seed = 4
-    beta = 3
+    seed = 8
+    beta = 0.1
     beta_o = beta
     beta_att = beta
 
-    num_feat_patterns = 1
-    max_sim_steps = 5
+    num_feat_patterns = 3
+    max_sim_steps = 50
     context_size = 2 ** positional_embedding_size
 
     normalize_weights_str = "np.sqrt(N*M)"
@@ -73,9 +73,6 @@ if __name__ == "__main__":
 
     print("Same weights", np.array_equal(HTInf.Wo, HT.Wo), np.array_equal(HTInf.Wv, HT.Wv), np.array_equal(HTInf.Wk, HT.Wk),
           np.array_equal(HTInf.Wq, HT.Wq))
-
-    print(HT.mf_statistics["mo"])
-    print(HTInf.mf_statistics["mo"])
 
     # Plotting
     print("Plotting statistics...")

models/HopfieldTransformerPEInfN.py
@@ -75,8 +75,8 @@ class HopfieldTransformerInfN:
 
             self.pair_corr_o_o /= semantic_embedding_bitsize
             self.pair_corr_o_v /= semantic_embedding_bitsize
-            self.pair_corr_o_q /= semantic_embedding_bitsize
             self.pair_corr_o_k /= semantic_embedding_bitsize
+            self.pair_corr_o_q /= semantic_embedding_bitsize
 
             if num_feat_patterns == 3:
                 self.quad_corr_o_o = np.zeros(num_feat_patterns)
@@ -95,7 +95,7 @@ class HopfieldTransformerInfN:
                 self.quad_corr_o_o /= semantic_embedding_bitsize
                 self.quad_corr_o_v /= semantic_embedding_bitsize
                 self.quad_corr_o_q /= semantic_embedding_bitsize
-                self.quad_corr_o_v /= semantic_embedding_bitsize
+                self.quad_corr_o_k /= semantic_embedding_bitsize
 
         elif correlations_from_weights == 0:  #  Normal weights and 4 correlations come from individual corrs
             sc = 0.5