cfc729af3ace34f8fb659f8a6c686b76467b4905
muxitox <angel.poc.lopez@gmail.com>
2024-06-11 14:00:23 +0200

    Update slurm scripts

---
+++ slurm_parallel/launcher_bifurcation_diagrams_inf_ctx_plot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SEED_LIST=(1)
+NUM_FEAT_PATTERNS_LIST=(3)
+POSITIONAL_EMBEDDING_SIZE_LIST=(2)
+INI_TOKEN_IDX_LIST=(0)
+CFG_PATH="cfgs/bif_diagram_inf_0.yaml"
+NUM_BIFURCATION_VALUES=4001
+
+for SEED in "${SEED_LIST[@]}"; do
+    for NUM_FEAT_PATTERNS in "${NUM_FEAT_PATTERNS_LIST[@]}"; do
+        for POSITIONAL_EMBEDDING_SIZE in "${POSITIONAL_EMBEDDING_SIZE_LIST[@]}"; do
+          for INI_TOKEN_IDX in "${INI_TOKEN_IDX_LIST[@]}"; do
+
+                LOAD_FROM_CONTEXT_MODE=1
+                source bifurcation_diagrams_out_inf_plot.sh $SEED $NUM_FEAT_PATTERNS $POSITIONAL_EMBEDDING_SIZE \
+                $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $LOAD_FROM_CONTEXT_MODE $CFG_PATH
+          done
+        done
+    done
+done
+
+
