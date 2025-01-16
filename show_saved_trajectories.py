a6c690d0408ebea032cf35569ed8c639c51bf79d
Ángel Poc <muxitox>
2025-01-16 12:17:32 +0100

    Remake the showing saved trajectories functionality to work with the new filsystem

--- show_saved_trajectories.py
+++ show_saved_trajectories.py
@@ -1,33 +1,24 @@
 import numpy as np
 import os
-from plotting.plotting import plot_save_statistics, plot_save_fft, plot_save_plane
-from bifurcation_diagrams import create_pathname
+from plotting.plotting import plot_save_statistics, plot_save_fft, plot_save_plane, plot_lyapunov_graphs
+import matplotlib.pyplot as plt
 from utils import create_dir_from_filepath
 import yaml
 
-def plotter(num_feat_patterns, seed, positional_embedding_size, context_size, ini_token_idx, worker_values_list, cfg,
-            stats_to_save_plot, beta_to_show, load_from_context_mode=0, plot_range=None):
+def plotter(worker_values_list, beta_to_show_idx, cfg, exp_dir, plot_range = None, save_not_plot=False):
 
     if cfg["save_non_transient"] == True:
         num_transient_steps_plot_arg = cfg["num_transient_steps"]
     else:
         num_transient_steps_plot_arg = 0
 
-    folder_path = create_pathname(num_feat_patterns, positional_embedding_size, context_size, worker_values_list,
-                                  load_from_context_mode, cfg)
+    save_basepath = (exp_dir + f"/saved_trajectories/")
+
+    if save_not_plot and (not os.path.exists(save_basepath)):
+        os.makedirs(save_basepath)
 
 
-
-    # Search idx for the beta nearest to beta_to_show
-    beta_to_show_idx = np.searchsorted(worker_values_list, beta_to_show)
-    print("beta", beta_to_show_idx, worker_values_list[beta_to_show_idx])
-
-    ini_token_mode_str = ""
-    ini_token_from_w = cfg["ini_token_from_w"]
-    if ini_token_from_w != 0:
-        ini_token_mode_str = f"-ini_token_from_w-{ini_token_from_w}"
-    stats_data_path = (folder_path + "/stats/seed-" + str(seed) + "-ini_token_idx-" + str(ini_token_idx)
-                       + ini_token_mode_str + "-beta_idx-" + str(beta_to_show_idx) + ".npz")
+    stats_data_path = (exp_dir + "/stats/beta_idx-" + str(beta_to_show_idx) + ".npz")
 
 
     # Load data
@@ -40,16 +31,14 @@ def plotter(num_feat_patterns, seed, positional_embedding_size, context_size, in
         stat_results_beta_list = data[f"{stat_name}_results_beta"]
         stat_results = stat_results_beta_list
 
-        plot_save_path_traj = (folder_path + f"/indiv_traj/seed-{str(seed)}/{stat_name}/beta-{beta_to_show}-ini_token_idx-" +
-                          str(ini_token_idx) + "-transient_steps-" + str(cfg["num_transient_steps"]) + image_format)
+        plot_save_path_traj = (save_basepath + f"/traj-beta-{beta_to_show}-transient_steps-" + str(cfg["num_transient_steps"]) + image_format)
 
-        plot_save_path_fft = (folder_path + f"/indiv_traj/seed-{str(seed)}/{stat_name}/fft-beta-{beta_to_show}-ini_token_idx-" +
-                          str(ini_token_idx) + "-transient_steps-" + str(cfg["num_transient_steps"]) + image_format)
+        plot_save_path_fft = (save_basepath + f"/fft-beta-{beta_to_show}-transient_steps-" + str(cfg["num_transient_steps"]) + image_format)
 
         plot_save_folder_path = os.path.dirname(plot_save_path_traj)
 
         # Create folder if it does not exist and we are saving the image
-        if cfg["save_not_plot"] and (not os.path.exists(plot_save_folder_path)):
+        if save_not_plot and (not os.path.exists(plot_save_folder_path)):
             os.makedirs(plot_save_folder_path)
 
         # title = (
@@ -63,77 +52,96 @@ def plotter(num_feat_patterns, seed, positional_embedding_size, context_size, in
 
         rg = range(plot_range[0], plot_range[1])
         show_1_feat = 1
-        plot_save_statistics(stat_results[rg, :], stat_name, num_feat_patterns,
+        plot_save_statistics(stat_results[rg, :], stat_name, cfg["num_feat_patterns"],
                              len(rg), min_num_step=cfg["num_transient_steps"] + plot_range[0],
-                             show_max_num_patterns=num_feat_patterns,
-                             save_not_plot=cfg["save_not_plot"], save_path=plot_save_path_traj, title=title,
+                             show_max_num_patterns=cfg["num_feat_patterns"],
+                             save_not_plot=save_not_plot, save_path=plot_save_path_traj, title=title,
                              plot_hilbert=False, show_1_feat=show_1_feat)
 
 
-        plot_save_fft(stat_results[num_transient_steps_plot_arg:, :], stat_name, num_feat_patterns,
+        plot_save_fft(stat_results[num_transient_steps_plot_arg:, :], stat_name, cfg["num_feat_patterns"],
                       cfg["max_sim_steps"] - cfg["num_transient_steps"],
-                      show_max_num_patterns=num_feat_patterns, save_not_plot=cfg["save_not_plot"],
+                      show_max_num_patterns=cfg["num_feat_patterns"], save_not_plot=save_not_plot,
                       save_path=plot_save_path_fft, title=title, show_1_feat=show_1_feat)
 
 
-    # 3 feats
-    stats_to_plot = [["mo_se"], ["mo_se"]]
-    feat_idx = [[0], [1]]
+    #################
+    ### Plot planes
+    #################
+    plot_save_path_plane = (save_basepath + f"/planes/"
+                            + f"/plane-beta-{worker_values_list[beta_to_show_idx]}" + "-transient_steps-" +
+                            str(cfg["num_transient_steps"]) + image_format)
 
-    plot_save_path_plane = (folder_path + f"/indiv_traj/latex/seed-{str(seed)}/planes"
-                            + f"/plane-beta-{beta_to_show}-ini_token_idx-" +
-                            str(ini_token_idx) + "-transient_steps-" + str(cfg["num_transient_steps"]) + image_format)
+    if save_not_plot:
+        create_dir_from_filepath(plot_save_path_plane)
 
-    create_dir_from_filepath(plot_save_path_plane)
+    nrows = 2
+    ncols = 3
+    fig, ax = plt.subplots(nrows, ncols, figsize=(8 * ncols, 8), constrained_layout=True)
 
-    stat_results_beta_list_0 = [data[f"{stats_to_plot[0][0]}_results_beta"]]
-    stat_results_beta_list_1 = [data[f"{stats_to_plot[1][0]}_results_beta"]]
+    feat_suffix = "_results_beta"
+    # Define the statistics you want to plot against each other
+    # In this case the feature mo with only the semantic information
+    stats_to_plot = [["mo_se", "mo_se"], ["mo_se", "mo_se"], ["mo_se", "mo_se"],
+                     ["att", "att"], ["att", "att"], ["att", "att"]]
+    # Define the index of the features you want to compare against each other
+    feat_idx = [[0, 1], [0, 2], [1, 2], [0, 1], [0, 2], [1, 2]]
 
-    plot_save_plane(stat_results_beta_list_0,
-                    stat_results_beta_list_1, cfg["max_sim_steps"] - cfg["num_transient_steps"], feat_idx,
-                    tag_names=stats_to_plot, save_path=plot_save_path_plane, save_not_plot=cfg["save_not_plot"],
-                    title=title)
+    flat_ax = ax.ravel()
+
+    for plot_i in range(len(flat_ax)):
+        # Load needed statistics
+        stat_results_beta_0 = data[stats_to_plot[plot_i][0] + feat_suffix][:, feat_idx[plot_i][0]]
+        stat_results_beta_1 = data[stats_to_plot[plot_i][1] + feat_suffix][:, feat_idx[plot_i][1]]
+
+        plot_save_plane(stat_results_beta_0,
+                        stat_results_beta_1, cfg["max_sim_steps"] - cfg["num_transient_steps"], feat_idx[plot_i],
+                        flat_ax[plot_i], tag_names=stats_to_plot[plot_i])
+
+    if save_not_plot:
+        fig.savefig(plot_save_path_plane, bbox_inches='tight')
+    else:
+        fig.show()
+
+    plt.close(fig)
+
+
+    # Plot Lyapunov
+    print("Lyapunov exponentsm (last 0 exponents correspond to positional encodings, we filter them when analyzing the data)")
+    print(data["S"])
+    print("Lyapunov exponents going to inf")
+    print(data["S_inf_flag"])
+
 
 
 if __name__ == "__main__":
+
+    exp_dir = "results_parallel_v3/results_small_sample_zoom2"
+
+    cfg_path = exp_dir + "/cfg.yaml"
     # Load cfg
-    cfg_path = 'cfgs/bif_diagram_inf_0_zoom-in.yaml'
     with open(cfg_path, 'r') as file:
         cfg = yaml.safe_load(file)
 
-    positional_embedding_size = 2
-    context_size = 2 ** positional_embedding_size
+    stats_to_save_plot = ["mo_se", "att"]
+    save_not_plot = False     # True -> save, False -> plot
 
-    num_bifurcation_values = 4001  # Number of x values to examine in the bifurcation diagram
-
-    worker_values_list = np.linspace(cfg["min_bifurcation_value"], cfg["max_bifurcation_value"],
-                                     num_bifurcation_values)  # Betas or Epsilon values
-
-    seed = 1  # List of seeds to review
-    num_feat_patterns = 3  # List of number of features for which to initialize the model
-    ini_token_idx = 0
-    load_from_last_chpt = True
-    beta_to_show = 0.5
-
+    # Define plotting range
     plot_window = 250
     offset = 10000 + plot_window
-    plot_range = [offset, offset + plot_window]    # Index of the range of steps want to plot within the trajectory
+    plot_range = [offset, offset + plot_window]  # Index of the range of steps want to plot within the trajectory
+
+    # Create x values for the bifurcation diagram.
+    worker_values_list = np.linspace(cfg["min_bifurcation_value"], cfg["max_bifurcation_value"],
+                                     cfg["num_bifurcation_values"])  # Betas or Epsilon values
+
+    # It will look for the most similar beta in the experiments
+    beta_to_show = 1.27382
+    # beta_to_show = 1.27387
+    # Search idx for the beta nearest to beta_to_show
+    beta_to_show_idx = np.searchsorted(worker_values_list, beta_to_show)
+    beta_to_show_idx = 657  # You can also define the index instead of searching
+    print("beta", beta_to_show_idx, worker_values_list[beta_to_show_idx])
 
 
-    if context_size > 2**positional_embedding_size:
-        raise Exception("The positional embedding cannot cover the whole context size.")
-    if cfg["num_transient_steps"] > cfg["max_sim_steps"]:
-        raise Exception("You cannot discard more timesteps than you are simulating.")
-    if plot_range[1] >=  cfg["max_sim_steps"] - cfg["num_transient_steps"]:
-        raise Exception("You are trying to plot more steps than simulated. Redefine plot_range.")
-
-    # stats_to_save_plot = ["mo", "mo_se", "mv", "mq", "mk", "att"]
-    stats_to_save_plot = ["mo_se"]
-
-    if load_from_last_chpt:
-        load_from_context_mode = 1
-    else:
-        load_from_context_mode = 0
-
-    plotter(num_feat_patterns, seed, positional_embedding_size, context_size, ini_token_idx, worker_values_list, cfg,
-            stats_to_save_plot, beta_to_show, load_from_context_mode=load_from_context_mode, plot_range=plot_range)
+    plotter(worker_values_list, beta_to_show_idx, cfg, exp_dir, plot_range=plot_range, save_not_plot=save_not_plot)
