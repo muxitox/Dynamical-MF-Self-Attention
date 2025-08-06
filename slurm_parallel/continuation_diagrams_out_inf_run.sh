1b0526ec844b5be67e45bb1057edb4599608b3c4
Ángel Poc <muxitox>
2025-08-06 16:48:07 +0200

    Start changing the scripts to perform the two sided continuation diagram

--- slurm_parallel/continuation_diagrams_out_inf_run.sh
+++ slurm_parallel/continuation_diagrams_out_inf_run.sh
@@ -24,7 +24,8 @@ INI_TOKEN_IDX=$5
 CFG_PATH_PRE=$6
 CFG_PATH_POST=$7
 EXP_DIR=$8
-WORKER_ID=$9
+CONT_ORDER=$9
+WORKER_ID=${10}
 
 if [ -z "$WORKER_ID" ]; # Check if variable is not defined, if not, define it from console args
 then
@@ -45,44 +46,69 @@ ARGS=" \
 --ini_token_idx=$INI_TOKEN_IDX \
 --cfg_path=$CFG_PATH_PRE \
 --exp_dir=$EXP_DIR \
+--cont_order=$CONT_ORDER \
 --worker_id=$WORKER_ID \
 "
 echo $ARGS >> ${LOG_PATH}.out
-#python bifurcation_diagrams_from_sh_run.py $ARGS >> ${LOG_PATH}.out 2>> ${LOG_PATH}.err
-#EXIT_CODE=$?
-##python ../bifurcation_diagrams_from_sh_run.py $ARGS
-#
-#if [[ "EXIT_CODE" -ne 0 ]]; then
-#  echo "Error in the execution. Exiting..." >> ${LOG_PATH}.out
-#  exit 1
-#fi
-#
-#ENDTIME=$(date +%s)
-#ELAPSEDTIME=$((end - start))
-## Convert to minutes (with decimal)
-#MINUTES=$(echo "scale=2; $ELAPSEDTIME/ 60" | bc)
-#
-#echo "It took $MINUTES minutes to complete this task..." >> ${LOG_PATH}.out
-#
-#if [[ "$WORKER_ID" -ne 1 ]]; then
-#
-#   echo "$WORKER_ID" >> ${LOG_PATH}.out
-#   WORKER_ID=$((WORKER_ID - 1))
-#   echo "$WORKER_ID" >> ${LOG_PATH}.out
-#
-#   echo "Queue next experiment with worker ID $WORKER_ID" >> ${LOG_PATH}.out
-#
-#   sbatch slurm_parallel/continuation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
-#                  $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_PRE $CFG_PATH_POST  \
-#                  $EXP_DIR $WORKER_ID
-#
-#else
-#
-#  echo "We have finished computing the first seed. Now queue the bifurcation diagram in parallel. " >> ${LOG_PATH}.out
-#
-#  sbatch --array=1-$NUM_BIFURCATION_VALUES slurm_parallel/bifurcation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
-#                $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_POST $EXP_DIR
-#fi
+python bifurcation_diagrams_from_sh_run.py $ARGS >> ${LOG_PATH}.out 2>> ${LOG_PATH}.err
+EXIT_CODE=$?
+#python ../bifurcation_diagrams_from_sh_run.py $ARGS
 
-sbatch --array=1-$NUM_BIFURCATION_VALUES slurm_parallel/bifurcation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
+if [[ "EXIT_CODE" -ne 0 ]]; then
+  echo "Error in the execution. Exiting..." >> ${LOG_PATH}.out
+  exit 1
+fi
+
+ENDTIME=$(date +%s)
+ELAPSEDTIME=$((end - start))
+# Convert to minutes (with decimal)
+MINUTES=$(echo "scale=2; $ELAPSEDTIME/ 60" | bc)
+
+echo "It took $MINUTES minutes to complete this task..." >> ${LOG_PATH}.out
+
+
+if  [[ "$CONT_ORDER" == "l" ]] && [[ "$WORKER_ID" -ne 1 ]]; then
+
+   WORKER_ID=$((WORKER_ID - 1))
+
+   echo "Queue next experiment with worker ID $WORKER_ID" >> ${LOG_PATH}.out
+
+   sbatch slurm_parallel/continuation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
+                  $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_PRE $CFG_PATH_POST  \
+                  $EXP_DIR $CONT_ORDER $WORKER_ID
+
+elif [[ "$CONT_ORDER" == "r" ]] && [[ "$WORKER_ID" -ne $NUM_BIFURCATION_VALUES ]]; then
+
+  WORKER_ID=$((WORKER_ID + 1))
+
+   echo "Queue next experiment with worker ID $WORKER_ID" >> ${LOG_PATH}.out
+
+   sbatch slurm_parallel/continuation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
+                  $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_PRE $CFG_PATH_POST  \
+                  $EXP_DIR $CONT_ORDER $WORKER_ID
+
+elif [[ "$CONT_ORDER" == "c" ]] && [[ "$WORKER_ID" -ne $NUM_BIFURCATION_VALUES ]]; then
+
+   WORKER_ID_L=$((WORKER_ID - 1))
+   WORKER_ID_R=$((WORKER_ID + 1))
+
+   echo "Queue next experiment with worker ID $WORKER_ID" >> ${LOG_PATH}.out
+
+   sbatch slurm_parallel/continuation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
+                  $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_PRE $CFG_PATH_POST  \
+                  $EXP_DIR "r" $WORKER_ID_R
+
+   sbatch slurm_parallel/continuation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
+                  $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_PRE $CFG_PATH_POST  \
+                  $EXP_DIR "l" $WORKER_ID_L
+
+else
+
+  echo "We have finished computing the first seed. Now queue the bifurcation diagram in parallel. " >> ${LOG_PATH}.out
+
+  sbatch --array=1-$NUM_BIFURCATION_VALUES slurm_parallel/bifurcation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
                 $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_POST $EXP_DIR
+fi
+
+#TODO:  change python code to assimilate the cont_order parameter.
+# TODO: check if there's any argument in this slurm version that allows me to wait for all jobs to finish given that they all have different ids
--- slurm_parallel/launcher_continuation_diagrams_inf_ctx_run.sh
+++ slurm_parallel/launcher_continuation_diagrams_inf_ctx_run.sh
@@ -7,6 +7,8 @@ INI_TOKEN_IDX_LIST=(0)
 CFG_PATH_PRE="cfgs/cont_diagram_pre_inf_0_zoom-in.yaml"
 CFG_PATH_POST="cfgs/cont_diagram_post_inf_0_zoom-in.yaml"
 NUM_BIFURCATION_VALUES=501
+CONT_ORDER="c"  # c: center and then divide, r: scan towards the right, l: scan towards the left
+INI_WORKER_ID=$NUM_BIFURCATION_VALUES
 
 
 SUFFIX=""
@@ -25,8 +27,8 @@ for SEED in "${SEED_LIST[@]}"; do
           for INI_TOKEN_IDX in "${INI_TOKEN_IDX_LIST[@]}"; do
 
                 DATE=$(date +%Y%m%d_%H%M%S)/
-#                EXP_DIR=$EXP_DIR_BASE/$DATE/
-                EXP_DIR="results_continuation/20250704_160253_zoom-2"
+                EXP_DIR=$EXP_DIR_BASE/$DATE/
+
                 # Create folders here to avoid errors creating them in parallel
                 mkdir -p  ${SUFFIX}${EXP_DIR}/stats/
                 mkdir -p ${SUFFIX}${EXP_DIR}/indiv_lowres_traj/lyapunov/
@@ -37,7 +39,7 @@ for SEED in "${SEED_LIST[@]}"; do
                 echo Num bifurcation values parallel $NUM_BIFURCATION_VALUES $NUM_BIFURCATION_VALUES
                 sbatch slurm_parallel/continuation_diagrams_out_inf_run.sh $SEED $NUM_FEAT_PATTERNS \
                   $POSITIONAL_EMBEDDING_SIZE $NUM_BIFURCATION_VALUES $INI_TOKEN_IDX $CFG_PATH_PRE  \
-                  $CFG_PATH_POST $EXP_DIR $NUM_BIFURCATION_VALUES
+                  $CFG_PATH_POST $EXP_DIR $CONT_ORDER $NUM_BIFURCATION_VALUES
 
 
             done
