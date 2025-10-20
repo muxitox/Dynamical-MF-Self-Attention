94c1bea07f5ed92ec6a744916c60c18526769fb2
Ángel Poc <muxitox>
2025-10-20 15:32:56 +0200

    New config

--- slurm_parallel/launcher_continuation_diagrams_inf_ctx_run.sh
+++ slurm_parallel/launcher_continuation_diagrams_inf_ctx_run.sh
@@ -4,8 +4,8 @@ SEED_LIST=(1)
 NUM_FEAT_PATTERNS_LIST=(3)
 POSITIONAL_EMBEDDING_SIZE_LIST=(2)
 INI_TOKEN_IDX_LIST=(0)
-CFG_PATH_PRE="cfgs/cont_diagram_pre_inf_0_zoom-in_less_steps.yaml"
-CFG_PATH_POST="cfgs/cont_diagram_post_inf_0_zoom-in.yaml"
+CFG_PATH_PRE="cfgs/cont_diagram_pre_inf_0.yaml"
+CFG_PATH_POST="cfgs/cont_diagram_post_inf_0.yaml"
 NUM_BIFURCATION_VALUES=501
 INI_WORKER_ID=501 # Number between 1 and NUM_BIFURCATION_VALUES
 
