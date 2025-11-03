0979b287a222225855887264e9e88c76b3cacde8
Ángel Poc <muxitox>
2025-11-03 17:19:03 +0100

    Update config

--- slurm_parallel/launcher_continuation_diagrams_inf_ctx_run.sh
+++ slurm_parallel/launcher_continuation_diagrams_inf_ctx_run.sh
@@ -4,7 +4,7 @@ SEED_LIST=(1)
 NUM_FEAT_PATTERNS_LIST=(3)
 POSITIONAL_EMBEDDING_SIZE_LIST=(2)
 INI_TOKEN_IDX_LIST=(0)
-CFG_PATH="cfgs/cont_diagram_pre_inf_0_zoom-in_less_steps.yaml"
+CFG_PATH="cfgs/cont_diagram_pre_inf_0_zoom-in.yaml"
 NUM_BIFURCATION_VALUES=501
 INI_WORKER_ID=501 # Number between 1 and NUM_BIFURCATION_VALUES
 
